13d863619a697dc48cd01c25264bf3bc5dcf0249
Oscar <oscarcalvert01@gmail.com>
2025-02-22 14:50:35 -0500

    created project skeleton

---
+++ server.js
@@ -0,0 +1,17 @@
+const PORT = 8989
+const express = require("express")
+const fs = require("fs")
+const path = require("path")
+const webroot = path.join(__dirname, 'public')
+
+const app = express()
+
+app.use(express.static(webroot))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(webroot, "template.html"))
+})
+
+app.listen(PORT, () => {
+    console.log("user")
+})
